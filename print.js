a999c404e91cdb05e8a68071719e493690fb819e
Parth Godase <parthgodase123@gmail.com>
2022-11-20 20:45:35 +0530

    git commit -m commit "c3"

--- print.js
+++ print.js
@@ -1,22 +1,42 @@
-var difficulty;
-var season;
-var budget;
-var region;
 
-function save()
+async function save()
 {
-    difficulty=docunment.getElementbyID("fdifficulty").value;
-    season=document.getElementById("fseason").value;
-    budget=docunment.getElementById("fbudget").value;
-    region=docunment.getElementById("fregion").value;
-    console.log('saved');
-    print();
+    var diff = document.getElementById("selectDifficulty").value;
+    var sn=document.getElementById("selectSeason").value;
+    var rgn=document.getElementById("selectRegion").value;
+    // budget=document.getElementById("fbudget").value;
+
+    console.log(diff, sn, rgn);
+
+    reqBody = {
+        trekParam: {
+            region: rgn,
+            season: sn,
+            difficulty: diff
+        }
+    }
+
+    head = {
+        "Accept": "*/*",
+        "Content-Type": "application/json"
+    };
+
+    const response = await fetch("http://localhost:5000/spots", {
+        "method": "POST",
+        "body": JSON.stringify(reqBody),
+        "headers": head
+    })
+    const data = await response.json();
+    const row = data[1];
+    console.log(row);
+
+    print(row);
 }
 
 
-function print()
+function print(arr)
 {
-    var arr = [1,2,3,4];
+    // var arr = [1,2,3,4];
     var html_string=`
 <table class="table">
   <thead>
@@ -32,13 +52,17 @@ function print()
   </thead>
   <tbody>
 `
-for (const i in arr) {
+for (let i=0; i<arr.length; i++) {
+    console.log(arr[i])
     html_string += `
     <tr>
-      <th scope="row">${i}</th>
-      <td> </td>
-      <td> </td>
-      <td> </td>
+      <th scope="row">${arr[i].T_id}</th>
+      <td>${arr[i].T_name}</td>
+      <td>${arr[i].T_season}</td>
+      <td>${arr[i].T_duration}</td>
+      <td>${arr[i].T_location}</td>
+      <td>${arr[i].R_id}</td>
+      <td>${arr[i].T_difficulty}</td>
     </tr>
     `
 }
